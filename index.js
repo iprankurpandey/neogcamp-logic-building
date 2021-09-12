b7970d273533b9a644abc0660a8bd249916047f8
Prankur Pandey <pprankur@gmail.com>
2021-09-12 16:12:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const input = require('readline-sync');
+const var1 = input.question('var1')
+const var2 = input.question('var1')
+const var3 = input.question('var1')
+const var4 = input.question('var1')
+const var5 = input.question('var1')
+const sum = Number(var1)+Number(var2)+Number(var3)+Number(var4)+Number(var5)
+console.log(Number(sum))
