a3b59827bc73163eb6ec7e2dc9818206ac589db2
Prankur Pandey <pprankur@gmail.com>
2021-09-13 06:55:05 +0000

    solving the neogcamp basic questions

---
+++ Q1.js
@@ -0,0 +1,10 @@
+console.log('Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.') 
+
+let arr = [ 5,13,07,21,48]
+let sum =0;
+for(i=0;i<arr.length;i++)
+{
+	sum = sum+arr[i]
+}
+
+
