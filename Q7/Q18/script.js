827f032a153e0b7a69f60100b8695f1794479376
iprankurpandey <pprankur@gmail.com>
2021-09-15 15:30:16 +0530

    solved q7

---
+++ Q7/Q18/script.js
@@ -0,0 +1,22 @@
+const button = document.querySelector('#loaded')
+const text = document.querySelector('#text')
+const image =  document.querySelector('#image')
+button.addEventListener('click',loading)
+
+function loading(){
+   if(text.style.display === 'none')  {
+    text.style.display ='block'
+   }
+   else{
+    text.style.display ='none'
+   }
+}
+
+// function loading(){
+//     if(image.style.display === 'none')  {
+//      image.style.display ='block'
+//     }
+//     else{
+//      image.style.display ='none'
+//     }
+//  }
