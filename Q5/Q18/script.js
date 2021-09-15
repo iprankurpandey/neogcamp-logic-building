34b66a842b67049cd51fe0daf8f9932af6c84b3a
iprankurpandey <pprankur@gmail.com>
2021-09-15 11:17:21 +0530

    solved Q5

---
+++ Q5/Q18/script.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('#inputbox')
+const redColor = document.querySelector('#red')
+const greenColor = document.querySelector('#green')
+const blueColor = document.querySelector('#blue')
+const resetB = document.querySelector('#reset')
+const outputBox = document.querySelector('#output')
+
+
+function red() {
+
+    outputBox.innerText = input.value;
+    outputBox.style.color= 'red'
+}
+
+function green() {
+    outputBox.innerText = input.value;
+    outputBox.style.color= 'green'
+}
+
+function blue() {
+    outputBox.innerText = input.value;
+    outputBox.style.color='blue'
+}
+
+function reset() {
+    outputBox.style.color=''
+}
+
+redColor.addEventListener('click', red)
+greenColor.addEventListener('click', green)
+blueColor.addEventListener('click', blue)
+resetB.addEventListener('click', reset)
