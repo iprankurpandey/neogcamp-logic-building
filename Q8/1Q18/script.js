08e42af6c9306f1505f3ef4551a82d2651adfc09
iprankurpandey <pprankur@gmail.com>
2021-09-19 17:02:27 +0530

    updated code

--- Q8/1Q18/script.js
+++ Q8/1Q18/script.js
@@ -1,1 +1,14 @@
-const 
+const input = document.querySelector('#input')
+console.log(input.value)
+const button = document.querySelector('#button')
+const output = document.querySelector('#output')
+const url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=' + input.value;
+
+function geturl() {
+    fetch(url)
+        .then(response => response.text())
+        .then(data =>
+            console.log(data));
+}
+
+button.addEventListener('click', geturl)
