1d4839d6b483fddb64b2023ed8cf7e490e415398
iprankurpandey <pprankur@gmail.com>
2021-09-20 20:43:54 +0530

    solved q18

--- Q18/Q18/script.js
+++ Q18/Q18/script.js
@@ -1,29 +1,12 @@
-const God1 = {
-    Name: 'ram',
-    power: '2500',
-    yuga: 'treta'
-}
+const input  =  require ('readline-sync')
+const newsInput = input.question('enter your news : ')
+const newsSource = input.question('enter news source ')
 
-const God2 = {
-    Name: 'krishna',
-    power: '2325',
-    yuga: 'dwapar'
-}
-
-power=(God1, God2)=>
+if (newsSource === 'facebook' || 'whastapp' || 'telegram')
 {
-    const god1Power= (God1.Name).length*35+Number(God1.power)
 
-    const god2Power= (God2.Name).length*35+Number(God2.power)
-
-    if (Number(god1Power) > Number(god2Power))
-    {
-        console.log('God 1 ,has more power')
-    } 
-    else 
-    {
-        console.log('God 2 ,has more power')
-    }
+	console.log('dont believe things on FB and Whatsapp')
 }
-
-power(God1,God2)
+else{
+	console.log('news is being evaulated')
+}
