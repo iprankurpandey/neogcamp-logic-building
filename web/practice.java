0132d8ab98ea21a371bfd995358f8ac7149f8e47
iprankurpandey <pprankur@gmail.com>
2021-09-22 16:11:11 +0530

    solved start program in java

---
+++ web/practice.java
@@ -0,0 +1,14 @@
+public class practice {
+    public static void main(String[] args) {
+        int num = 5, i, j;
+        for (i = 0; i < num; i++) 
+        {
+            for (j = 0; j <= i; j++)
+            {
+                System.out.print('*');
+            }
+            System.out.println();
+
+        }
+    }
+} 
