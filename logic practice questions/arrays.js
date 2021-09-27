356ec8cc658424c4ec9a1b0bff40abebaffebbe5
iprankurpandey <pprankur@gmail.com>
2021-09-27 14:30:39 +0530

    solved

--- logic practice questions/arrays.js
+++ logic practice questions/arrays.js
@@ -14,3 +14,23 @@ for(i=0;i<array.length;i++){
 	average = sum/array.length;
 	
 }
+
+// Find maximum and minimum of an array.
+
+const array = [10,4,5,2,5,6,9]
+const min = Math.min(...array)
+console.log(min)
+const max=Math.max(...array)
+console.log(max)
+// let max =array[0]
+// let min = array[0]
+// for(i=0;i<array;i++){
+// 	array[i]
+// 	if(array[i]>max){
+// 		max = array[i]
+// 	}
+//  if(array[i]<min){
+// 	 min = array[i]
+//  }
+//  console.log(min)
+// }
