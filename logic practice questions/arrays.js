e6052dea3acf7c528501db6bc650377106cab613
iprankurpandey <pprankur@gmail.com>
2021-09-27 14:30:05 +0530

    solved

--- logic practice questions/arrays.js
+++ logic practice questions/arrays.js
@@ -4,3 +4,13 @@ let sum =0;
 for(i=0;i<array.length;i++){
 	sum +=array[i]
 }
+
+// Find average of an array and display the output.
+const array = [10,4,5,2,5,6,9]
+let sum =0;
+let average = 0;
+for(i=0;i<array.length;i++){
+	sum +=array[i]
+	average = sum/array.length;
+	
+}
