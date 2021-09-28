6be820423b5c1db209b0efa3b4112020edce50cd
iprankurpandey <pprankur@gmail.com>
2021-09-28 17:42:11 +0530

    solved

--- logic practice questions/functions.js
+++ logic practice questions/functions.js
@@ -8,3 +8,14 @@ function power(a,b){
 	return power
 }
 power(5,3)
+
+// Given length of a regular hexagon, your function should return area of the hexagon.
+// Example:// Input: areaOfHexagon(10) ––> Output: 259.80
+ 
+ let s ;
+ function areaOfHexagonre(s){
+	const area = ((3 * Math.sqrt(3) * (s * s)) / 2); 
+	return area;   
+
+ }
+areaOfHexagonre(10)
