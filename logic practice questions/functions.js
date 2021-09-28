1928afc7a5db82b3b6e9096f606eff412aa651f4
iprankurpandey <pprankur@gmail.com>
2021-09-28 17:40:47 +0530

    solved

---
+++ logic practice questions/functions.js
@@ -0,0 +1,10 @@
+
+// Given a and b, your function should return the value of ab
+// Example:Input: power(2,3) ––> Output: 8
+
+let a,b=0;
+function power(a,b){
+	const power = Math.pow(a,b)
+	return power
+}
+power(5,3)
