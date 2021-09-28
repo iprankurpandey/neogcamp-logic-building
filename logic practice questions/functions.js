e85f49587188e44ded7c9a9151b959160e090e87
iprankurpandey <pprankur@gmail.com>
2021-09-28 17:43:29 +0530

    solved

--- logic practice questions/functions.js
+++ logic practice questions/functions.js
@@ -46,3 +46,22 @@ function min(...args) {
 }
 
 min(1,2,3,4444,5,78,-1)
+
+// Given n numbers, your function should return the maximum of them all. The number of parameters won't be accepted from user.
+// Example:
+// Input: findMax(3,5) ––> Output: 5
+// Input: findMax(3,5,9,1) ––> Output: 9
+// (Hint: Lookup rest parameters in JavaScript)
+
+
+function max(...args) {
+    var maxValue = args[0];
+    for (var i = 1; i < args.length; i++) {
+        if (maxValue < args[i]) {
+            maxValue = args[i];
+        }
+    }
+    return maxValue;
+}
+
+max(1,2,3,4444,5,78,101525)
