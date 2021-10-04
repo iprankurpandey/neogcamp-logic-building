e910bf1d5ca491c4e7141d5b2519be56d2330a4c
iprankurpandey <pprankur@gmail.com>
2021-10-04 14:45:16 +0530

    solved

---
+++ JSPractice/app.js
@@ -0,0 +1,30 @@
+// let x = 10 
+// let x = 20
+// console.log(x)
+
+// var  x = 10 
+// var  x = 20
+// console.log(x)
+
+// const x = 10 
+// const x = 20
+// console.log(x)
+
+// const number = [1,2,3,4,5,6]
+// number [0] = 8
+// console.log(number)
+// number.push(17)
+// console.log(number)
+
+const car = {type:"Fiat", model:"500", color:"white"};
+car.color = 'red'
+car.owner = 'lala'
+console.log(car.color)
+console.log(car)
+
+https://www.simplilearn.com/tutorials/devops-tutorial/what-is-devops?source=sl_frs_eng_user_clicks_on_watch_tutorial
+let uName;
+console.log(uName)
+if (uName === undefined){
+    alert('please update name ')
+}
