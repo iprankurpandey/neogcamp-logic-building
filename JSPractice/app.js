9f8d4049c3fa178486dcf140093993275e527ff6
iprankurpandey <pprankur@gmail.com>
2021-10-07 21:24:01 +0530

    solved

--- JSPractice/app.js
+++ JSPractice/app.js
@@ -11,16 +11,19 @@
 // console.log(x)
 
 // const number = [1,2,3,4,5,6]
+
 // number [0] = 8
 // console.log(number)
 // number.push(17)
 // console.log(number)
 
-const car = {type:"Fiat", model:"500", color:"white"};
+const car = {
+    type:"Fiat", 
+    model:"500", 
+    color:"white"};
 car.color = 'red'
 car.owner = 'lala'
-console.log(car.color)
-console.log(car)
+
 console.table(car)
 
 // let uName;
