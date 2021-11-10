ddaa408c05c0fae301fca2b02ffdfb426c2a8750
iprankurpandey <pprankur@gmail.com>
2021-11-10 17:37:08 +0530

    updated code

--- neog/Q3/Q18/script.js
+++ neog/Q3/Q18/script.js
@@ -1,26 +1,47 @@
-const input = document.querySelector('#inputbox')
-const addition = document.querySelector('#add')
-const subtraction = document.querySelector('#subtract')
-const resetB = document.querySelector('#reset')
-const outputBox = document.querySelector('#output')
+const input1 = document.querySelector('#input1')
+const input2 = document.querySelector('#input2')
+const add = document.querySelector('#add')
+const subtract = document.querySelector('#subtract')
+const multiply = document.querySelector('#multiply')
+const divide = document.querySelector('#divide')
+const reset = document.querySelector('#reset')
+const text = document.querySelector('#text')
+let result = 0;
 
+function addition() {
+    result = Number(input1.value) + Number(input2.value);
 
-function sum() {
-  
-    outputBox.innerText = input.value;
-    outputBox.style.fontSize = '4rem'
-    
+    text.innerHTML = `the result is : ${result}`
 }
 
-function subtract() {
-        outputBox.innerText = input.value;
-    outputBox.style.fontSize = '2rem'
+function subtraction() {
+    result = Number(input1.value) - Number(input2.value);
+    text.innerHTML = `the result is : ${result}`
 }
 
-function reset() {
-    outputBox.style.fontSize= ''
+function multiplication() {
+    result = Number(input1.value) * Number(input2.value);
+    text.innerHTML = `the result is : ${result}`
 }
 
-addition.addEventListener('click', sum)
-subtraction.addEventListener('click', subtract)
-resetB.addEventListener('click', reset)
+function division() {
+    result = Number(input1.value) / Number(input2.value);
+
+    if (isNaN(result)) {
+        text.innerHTML = `the result is : dont divide zero`
+    } else {
+        text.innerHTML = `the result is : ${result}`
+    }
+}
+
+function reset1() {
+    input1.value = ''
+    input2.value = ''
+    text.innerHTML = `the result is : reset`
+}
+
+add.addEventListener('click', addition)
+subtract.addEventListener('click', subtraction)
+multiply.addEventListener('click', multiplication)
+divide.addEventListener('click', division)
+reset.addEventListener('click', reset1)
